815072fc89906bb66d309d3263cea6c65c70a949
Kemeker <kemeker@gmail.com>
2024-02-01 21:19:41 -0300

    feat: atualizado codigo, layout 90%

--- aguas-jupia/src/App.jsx
+++ aguas-jupia/src/App.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import fundoImage from '../src/assets/fundo3.jpeg'
 import './App.css'
-import SocialCard from '../src/components/socialCard';
+import logoImage from '../src/assets/AguaAmericas.jpg'
+
 
 function App() {
   
 
   return (
     <>
-    <div class="card-body">
-      <img src={fundoImage} alt="fundo3"/>
-      <SocialCard/>
-     </div>
+      <div className='container mx-auto'>
+        <div className='card-body'>
+          <img className='logo' src={logoImage} alt="logo"/>
+          
+          <ul className='"social-links" class="list-unstyled"'>
+            <li className='social-link fonte-ferradura'>
+              <a href="#" className='btn btn-primary fonte-ferradura'>Fonte Ferradura</a>
+            </li>
+            <li className='social-link wpp'>
+            <a href="#" className='btn btn-primary wpp'>Whatsapp</a>
+            </li>
+            <li className='social-link insta'>
+            <a href="#" className='btn btn-primary insta'>Instagram</a>
+            </li>
+            <li className='social-link history'>
+            <a href="#" className='btn btn-primary history'>Historia</a>
+            </li>
+          </ul>
+        </div>
+      </div>    
+      
+            
+          
+      
     </>
   )
 }
