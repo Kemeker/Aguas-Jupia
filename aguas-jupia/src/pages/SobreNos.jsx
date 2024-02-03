72d1d908566a6f5255b03d0f97591c19c475e5a1
Kemeker <kemeker@gmail.com>
2024-02-03 14:36:48 -0300

    Feat: criado slides para passar as fotos na pagina Sobre nos

--- aguas-jupia/src/pages/SobreNos.jsx
+++ aguas-jupia/src/pages/SobreNos.jsx
@@ -8,6 +8,8 @@ import Drone2 from '../assets/Fotos/drone2.jpeg'
 import Drone from '../assets/Fotos/drone.jpeg'
 import Bomba from '../assets/Fotos/bomba.jpeg'
 import Bomba2 from '../assets/Fotos/bomba2.jpeg'
+import Agua from '../assets/Fotos/agua.jpeg'
+import Letreiro from '../assets/Fotos/ferradura.jpeg'
 
 
 
@@ -18,16 +20,28 @@ function SobreNos(){
                 <button type='button' data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type='button' data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 <button type='button' data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type='button' data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                <button type='button' data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                <button type='button' data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                <button type='button' data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
             </div>
             <div className='carousel-inner'>
                 <div className='carousel-item active'>
-                    <img className="d-block" src={Drone2} alt="analise" />
+                    <img className="d-block" src={Drone2} alt="drone" />
                 </div>
                     
                 <div className="carousel-item">
-                    <img className="d-block" src={Drone} alt="analise" />
+                    <img className="d-block" src={Drone} alt="drone" />
                 </div>
 
+                <div className='carousel-item'>
+                    <img className="d-block" src={Bomba} alt="bomba d'agua" />
+                </div>
+
+                <div className='carousel-item'>
+                    <img className="d-block" src={Bomba2} alt="bomba d'agua" />
+                </div>
+                
                 <div className='carousel-item'>
                     <img className="d-block" src={Analise} alt="analise" />
                 </div>
@@ -37,10 +51,11 @@ function SobreNos(){
                 </div>
                 
                 <div className='carousel-item'>
-                    <img className="d-block" src={Bomba} alt="analise" />
+                    <img className="d-block" src={Agua} alt="agua" />
                 </div>
+
                 <div className='carousel-item'>
-                    <img className="d-block" src={Bomba2} alt="analise" />
+                    <img className="d-block" src={Letreiro} alt="letreiro" />
                 </div>
 
             </div>
