7cb7b7283cb2e94c4204f12d3e99b070b86077a1
Kemeker <kemeker@gmail.com>
2024-02-03 14:24:27 -0300

    Feat: adicionado novas imagens a pagina QuemSomosNos

--- aguas-jupia/src/pages/SobreNos.jsx
+++ aguas-jupia/src/pages/SobreNos.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../css/SobreNos.css'
 import Analise from '../assets/Fotos/analise.jpeg'
+import Analise2 from '../assets/Fotos/analise2.jpeg'
 import Drone2 from '../assets/Fotos/drone2.jpeg'
 import Drone from '../assets/Fotos/drone.jpeg'
+import Bomba from '../assets/Fotos/bomba.jpeg'
+import Bomba2 from '../assets/Fotos/bomba2.jpeg'
 
 
 
@@ -10,56 +15,45 @@ function SobreNos(){
     return(
         <div id="carouselExampleIndicators" className="carousel slide">
             <div className='carousel-indicators'>
-                <button type='button' data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type='button' data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type='button' data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 <button type='button' data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
             <div className='carousel-inner'>
                 <div className='carousel-item active'>
-                    <img className='class="d-block w-100"' src={Drone2} alt="analise" />
+                    <img className="d-block" src={Drone2} alt="analise" />
                 </div>
                     
+                <div className="carousel-item">
+                    <img className="d-block" src={Drone} alt="analise" />
+                </div>
+
                 <div className='carousel-item'>
-                    <img className='class="d-block w-100"' src={Drone2} alt="drone" />
+                    <img className="d-block" src={Analise} alt="analise" />
+                </div>
+
+                <div className='carousel-item'>
+                    <img className="d-block" src={Analise2} alt="analise" />
+                </div>
+                
+                <div className='carousel-item'>
+                    <img className="d-block" src={Bomba} alt="analise" />
                 </div>
                 <div className='carousel-item'>
-                    <img className='class="d-block w-100"' src={Drone} alt="drone" /> 
+                    <img className="d-block" src={Bomba2} alt="analise" />
                 </div>
+
             </div>
             <button className='carousel-control-prev' type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                 <span className='carousel-control-prev-icon' aria-hidden="true"></span>
-                <span className='visually-hidden'></span>
+                <span className='visually-hidden'>Previous</span>
             </button>
             <button className='carousel-control-next' type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
                 <span className='carousel-control-next-icon' aria-hidden="true"></span>
-                <span className='visually-hidden'></span>
+                <span className='visually-hidden'>Next</span>
             </button>
         </div>
-        
-
-                
-
-
-
-            
-
-
-        
-        
     )
-
-        
-        
-
-
-
-        
-
-
-
-
-        
-    
 }
 
 export default SobreNos
