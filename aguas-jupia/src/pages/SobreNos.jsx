78e6d45c957a90a6b783ff4a962d94a3c4a8813f
Kemeker <kemeker@gmail.com>
2024-02-03 11:17:23 -0300

    Feat: Ajustes no link do history

--- aguas-jupia/src/pages/SobreNos.jsx
+++ aguas-jupia/src/pages/SobreNos.jsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-import React from "react";
-// No arquivo onde você está usando o carrossel
-
-
-=======
 import React from 'react'
 import '../css/Home.css'
->>>>>>> 4ad81944e3784080d1f3d4bf47e2aa921d0bc110
-
 
 function SobreNos(){
     return(
-<<<<<<< HEAD
+
         <main className="card">
             <h1>teste</h1>
 
@@ -21,10 +13,7 @@ function SobreNos(){
         </main>
     )
 }
-=======
-        <h1>Sobre nós</h1>
-    )
-}
 
->>>>>>> 4ad81944e3784080d1f3d4bf47e2aa921d0bc110
+
+
 export default SobreNos
